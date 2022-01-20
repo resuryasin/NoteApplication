d6d1b439e1ee2ed54cba40165003698a43f179b5
resuryasin <672016198@student.uksw.edu>
2022-01-20 22:05:50 +0700

    Add AddEditNoteActivity activity

--- app/src/main/java/re/sin/noteapplication/MainActivity.kt
+++ app/src/main/java/re/sin/noteapplication/MainActivity.kt
@@ -1,11 +1,86 @@
 package re.sin.noteapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import re.sin.noteapplication.view.NotesRVAdapter
+import re.sin.noteapplication.entities.Notes
+import re.sin.noteapplication.viewmodel.NotesViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NotesRVAdapter.NotesClickInterface, NotesRVAdapter.NotesClickDeleteInterface {
+
+    // on below line we are creating a variable
+    // for our recycler view, exit text, button and viewmodel.
+    lateinit var viewModal: NotesViewModel
+    lateinit var notesRV: RecyclerView
+    lateinit var addFAB: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // on below line we are initializing
+        // all our variables.
+        notesRV = findViewById(R.id.notesRV)
+        addFAB = findViewById(R.id.idFAB)
+
+        // on below line we are setting layout
+        // manager to our recycler view.
+        notesRV.layoutManager = LinearLayoutManager(this)
+
+        // on below line we are initializing our adapter class.
+        val noteRVAdapter = NotesRVAdapter(this, this, this)
+
+        // on below line we are setting
+        // adapter to our recycler view.
+        notesRV.adapter = noteRVAdapter
+
+        // on below line we are
+        // initializing our view modal.
+        viewModal = ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        ).get(NotesViewModel::class.java)
+
+        // on below line we are calling all notes method
+        // from our view modal class to observer the changes on list.
+        viewModal.allNotes.observe(this, Observer { list ->
+            list?.let {
+                // on below line we are updating our list.
+                noteRVAdapter.updateList(it)
+            }
+        })
+        addFAB.setOnClickListener {
+            // adding a click listener for fab button
+            // and opening a new intent to add a new note.
+            val intent = Intent(this@MainActivity, AddEditNoteActivity::class.java)
+            startActivity(intent)
+            this.finish()
+        }
+    }
+
+    override fun onNoteClick(notes: Notes) {
+        // opening a new intent and passing a data to it.
+        val intent = Intent(this@MainActivity, AddEditNoteActivity::class.java)
+        intent.putExtra("noteType", "Edit")
+        intent.putExtra("noteTitle", notes.noteTitle)
+        intent.putExtra("noteDescription", notes.noteDescription)
+        intent.putExtra("noteId", notes.id)
+        startActivity(intent)
+        this.finish()
+    }
+
+    override fun onDeleteIconClick(notes: Notes) {
+        // in on note click method we are calling delete
+        // method from our view modal to delete our not.
+        viewModal.deleteNote(notes)
+        // displaying a toast message
+        Toast.makeText(this, "${notes.noteTitle} Deleted", Toast.LENGTH_LONG).show()
     }
 }
