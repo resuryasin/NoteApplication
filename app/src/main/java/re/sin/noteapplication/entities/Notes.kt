0ad6dfca2d5e738b58668a7223e2ab83e5afc4f0
resuryasin <672016198@student.uksw.edu>
2022-01-20 21:58:16 +0700

    Add `Notes` entity

---
+++ app/src/main/java/re/sin/noteapplication/entities/Notes.kt
@@ -0,0 +1,14 @@
+package re.sin.noteapplication.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notesTable")
+class Notes (
+    @ColumnInfo(name = "title") val noteTitle :String,
+    @ColumnInfo(name = "description") val noteDescription :String,
+    @ColumnInfo(name = "timestamp") val timeStamp :String
+    ) {
+    @PrimaryKey(autoGenerate = true) var id = 0
+}
