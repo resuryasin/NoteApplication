b87dbf919dea11bf01d53c4420eeef68577882b8
resuryasin <672016198@student.uksw.edu>
2022-01-20 22:05:01 +0700

    Add `Notes` viewmodel

---
+++ app/src/main/java/re/sin/noteapplication/viewmodel/NotesViewModel.kt
@@ -0,0 +1,34 @@
+package re.sin.noteapplication.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import re.sin.noteapplication.NotesDatabase
+import re.sin.noteapplication.repositories.NotesRepository
+import re.sin.noteapplication.entities.Notes
+
+class NotesViewModel(application: Application) :AndroidViewModel(application) {
+    val allNotes : LiveData<List<Notes>>
+    val repository : NotesRepository
+
+    init {
+        val dao = NotesDatabase.getDatabase(application).getNotesDao()
+        repository = NotesRepository(dao)
+        allNotes = repository.allNotes
+    }
+
+    fun deleteNote (notes: Notes) = viewModelScope.launch(Dispatchers.IO) {
+        repository.delete(notes)
+    }
+
+    fun updateNote(notes: Notes) = viewModelScope.launch(Dispatchers.IO) {
+        repository.update(notes)
+    }
+
+    fun addNote(notes: Notes) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(notes)
+    }
+}
